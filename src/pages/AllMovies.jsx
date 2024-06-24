0ebbe0f9d80c26cfe50fbce8c9bea64b930d5064
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2024-06-24 20:04:02 +0530

    Update AllMovies.jsx

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -11,7 +11,8 @@ const AllMovies = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [movies, setMovies] = useState([]);
   const { data, loading } = useFetch(
-    `${baseUrl}/trending/movie/week?${apiKey}&language=hi-IN&page=${currentPage}`
+    `${baseUrl}/trending/movie/?${apiKey}&language=en-US&region=IN&sort_by=popularity.desc&primary_release_year=2018&with_original_language=hi&page=${currentPage}`
+    //`${baseUrl}/trending/movie/week?${apiKey}&language=en-US&page=${currentPage}`
   );
 
   useEffect(() => {
